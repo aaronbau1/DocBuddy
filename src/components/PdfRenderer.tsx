2c0dbfbb9ab47e6a48a9ad4c9e6148986409c826
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-18 17:18:02 -0500

    fixed pdf pagination issue and added page validation

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -12,6 +12,12 @@ import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { useState } from 'react';
 
+import {useForm} from 'react-hook-form';
+import { z } from 'zod';
+
+import {zodResolver} from '@hookform/resolvers/zod';
+import { cn } from '@/lib/utils';
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -24,28 +30,72 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
   const {toast} = useToast();
 
   const [numPages, setNumPages] = useState<number>();
+  const [currPage, setCurrPage] = useState<number>(1);
+
+  const CustomPageValidator = z.object({
+    page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
+  })
+
+  type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+  const {register, handleSubmit, formState: {errors}, setValue} = useForm<TCustomPageValidator>({
+    defaultValues: {
+      page: '1'
+    },
+    resolver: zodResolver(CustomPageValidator)
+  })
   
   const {width, ref} = useResizeDetector();
+
+  const handlePageSubmit=(({page}: TCustomPageValidator) => {
+    setCurrPage(Number(page));
+    setValue('page', String(page));
+  })
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">
-          <Button variant = 'ghost' aria-label='previous page'>
-            <ChevronDown className='h-4 w-4' />
+          <Button 
+            variant = 'ghost'
+            aria-label='previous page'
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1));
+              setValue('page', String(currPage - 1));
+            }}
+          >
+            <ChevronUp className='h-4 w-4' />
           </Button>
 
           <div className='flex items-center gap-1.5'>
-            <Input className='w-12 h-8'/>
+            <Input {...register('page')} className={cn('w-12 h-8', errors.page && 'focus-visible:ring-red-500')}
+              onKeyDown={(e) => {
+                if(e.key === 'Enter') {
+                  handleSubmit(handlePageSubmit)()
+                }
+              }}
+            />
             <p className='text-zinc-700 text-sm space-x-1'>
               <span>/</span>
               <span>{numPages ?? 'x'}</span>
             </p>
           </div>
 
-          <Button variant = 'ghost' aria-label='previous page'>
-            <ChevronUp className='h-4 w-4' />
+          <Button variant = 'ghost'
+            aria-label='previous page'
+            disabled={numPages === undefined || currPage === numPages}
+            onClick={() => {
+              setCurrPage((prev) => (prev + 1 > numPages! ? numPages! : prev + 1));
+              setValue('page', String(currPage + 1));
+            }}
+          >
+            <ChevronDown className='h-4 w-4' />
           </Button>
+        </div>
+
+        <div className='space-x-2'>
+          
         </div>
       </div>
       
@@ -67,7 +117,7 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
           className='max-h-full'
           onLoadSuccess={({numPages}) => setNumPages(numPages)}
           >
-            <Page width={width ? width : 1} pageNumber={1} />
+            <Page width={width ? width : 1} pageNumber={currPage} />
           </Document>
         </div>
       </div>
