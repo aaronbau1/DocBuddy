6092270de84483d13cf989898d5e131a1f164d49
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-19 09:15:26 -0500

    added pdf renderer zoom and rotation features

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -4,7 +4,7 @@ import {Document, Page, pdfjs} from 'react-pdf';
 
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
-import { ChevronDown, ChevronUp, Loader2 } from 'lucide-react';
+import { ChevronDown, ChevronUp, Loader2, RotateCw, Search } from 'lucide-react';
 import { useToast } from './ui/use-toast';
 
 import{useResizeDetector} from 'react-resize-detector';
@@ -17,7 +17,9 @@ import { z } from 'zod';
 
 import {zodResolver} from '@hookform/resolvers/zod';
 import { cn } from '@/lib/utils';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
 
+import SimpleBar from 'simplebar-react';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -31,6 +33,8 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
 
   const [numPages, setNumPages] = useState<number>();
   const [currPage, setCurrPage] = useState<number>(1);
+  const [scale, setScale] = useState<number>(1);
+  const [rotation, setRotation] = useState<number>(0);
 
   const CustomPageValidator = z.object({
     page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
@@ -95,31 +99,61 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
         </div>
 
         <div className='space-x-2'>
-          
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className='gap-1.5' aria-label='zoom' variant='ghost'>
+                <Search className='h-4 w-4'/>
+                {scale * 100}%<ChevronDown className='h-3 w-3 opacity-50'/>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem onSelect={() => setScale(0.67)}>
+                67%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(1.0)}>
+                100%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                150%
+              </DropdownMenuItem>
+              <DropdownMenuItem onSelect={() => setScale(2)}>
+                200%
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <Button 
+            aria-label='rotate 90 degrees' 
+            onClick={() => setRotation((prev) => prev + 90)}
+          >
+            <RotateCw className='h-4 w-4'/>
+          </Button>
         </div>
       </div>
       
       <div className="flex-1 w-full max-h-screen">
-        <div ref={ref}>
-          <Document loading={
-            <div className='flex justify-center'>
-              <Loader2 className='my-24 h-6 w-6 animate-spin' />
-            </div>
-          }
-          onLoadError={() => {
-            toast({
-              title: 'Error loading PDF',
-              description: 'Please try again later',
-              variant: 'destructive',
-            })
-          }} 
-          file={url}
-          className='max-h-full'
-          onLoadSuccess={({numPages}) => setNumPages(numPages)}
-          >
-            <Page width={width ? width : 1} pageNumber={currPage} />
-          </Document>
-        </div>
+        <SimpleBar autoHide={false} className='max-h-[calc(100vh-10rem)]'>
+          <div ref={ref}>
+            <Document loading={
+              <div className='flex justify-center'>
+                <Loader2 className='my-24 h-6 w-6 animate-spin' />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: 'Error loading PDF',
+                description: 'Please try again later',
+                variant: 'destructive',
+              })
+            }} 
+            file={url}
+            className='max-h-full'
+            onLoadSuccess={({numPages}) => setNumPages(numPages)}
+            >
+              <Page width={width ? width : 1} pageNumber={currPage} scale={scale} rotate={rotation}/>
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   )
