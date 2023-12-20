331e729f3c545d3c76e9125931b5a2a25b9e93e4
aaronbau1 <112639833+aaronbau1@users.noreply.github.com>
2023-12-19 19:33:46 -0500

    Added loading state to PDF ChatWrapper

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import { trpc } from "@/app/_trpc/client";
 import ChatInput from "./ChatInput";
 import Messages from "./Messages";
+import { Loader2 } from "lucide-react";
 
 interface ChatWrapperProps {
   fileId: string
@@ -17,6 +20,21 @@ const ChatWrapper =  ({fileId}: ChatWrapperProps) => {
         data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
     }
   );
+
+  if(true) {
+    return (
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 flex justify-center items-center flex-col mb-28">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 text-blue-500 animate-spin"/>
+          <h3 className="font-semibold text-xl">...Loading</h3>
+          <p className="text-zinc-500 text-sm">
+            We&apos;re preparing your PDF
+          </p>
+        </div>
+      </div>
+    </div>
+  )}
   
   return (
     <div className="relative min-f full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
